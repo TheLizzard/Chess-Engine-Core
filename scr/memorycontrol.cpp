a960238105bc3aefc31f71d4fbb1027488320c82
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-10-31 20:26:53 +0000

    Fixed some bugs

--- scr/memorycontrol.cpp
+++ scr/memorycontrol.cpp
@@ -1,29 +1,26 @@
 #pragma once
 #include <iostream>
-//: public SuperClass
+
+
 using namespace std;
+
 
 long long int NEW_VAR_ID = 0;
 
+
+//: public SuperClass
 class SuperClass{
     public:
-        void operator delete(void* self, size_t size){
-            char* _self = (char*)self;
-            //int code = _self[size];
-            //cout << "delete (code = " << code << ")\n";
-            NEW_VAR_ID--;
-            cout << "delete " << NEW_VAR_ID << " objects remaining\n";
-            delete _self;
-        }
-    
         void* operator new(size_t size){
-            void* ptr = new char[size+4];
-            //char* as_str = (char*)ptr;
-            //as_str[size] = NEW_VAR_ID;
-            //cout << "new (code = " << NEW_VAR_ID << ")\n";
+            void* ptr = new char[size];
             NEW_VAR_ID++;
             cout << "new " << NEW_VAR_ID << " objects remaining\n";
             return ptr;
         }
-};
 
+        void operator delete(void* ptr, size_t size){
+            NEW_VAR_ID--;
+            cout << "delete " << NEW_VAR_ID << " objects remaining\n";
+            //free(ptr);
+        }
+};
