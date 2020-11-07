64bec902b5372883238205daf89a43b4806bcbab
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-11-07 22:34:18 +0000

    Added some comments

--- scr/memorycontrol.cpp
+++ scr/memorycontrol.cpp
@@ -12,22 +12,45 @@ long long int NEW_VAR_ID = 0;
 class SuperClass{
     public:
         void* operator new(size_t size){
+            /*
+            Creates a new object and prints to stdout the
+            number of objects remaining (on the heap only)
+            */
+
+            // Reserve memory:
             void* ptr = new char[size];
+            // Increment counter
             NEW_VAR_ID++;
             cout << "new " << NEW_VAR_ID << " objects remaining\n";
             return ptr;
         }
 
         void operator delete(void* ptr, size_t size){
+            /*
+            Deletes the object from the heap and prints to stdout
+            the number of objects remaining (on the heap only)
+            */
+
+            // Decrement counter
             NEW_VAR_ID--;
             cout << "delete " << NEW_VAR_ID << " objects remaining\n";
-            //free(ptr);
+            free(ptr);
         }
 };
 
 
-void is_on_heap(long long int variable_testing){
-    int heap_variable;int* pointer = &heap_variable;
-    bool on_heap = (long long int)pointer<variable_testing;
-    cout << "On heap = " << (on_heap?"true":"false") << "\n";
+bool is_on_heap(long long int variable_testing){
+    /*
+    Checks if the pointer given to it (as a long long int) is
+    on the heap or on the stack. It does that by createing a
+    new object that is on the stack and comparing its location
+    to the pointer passed in.
+    */
+
+    // Create the stack variable
+    int stack_variable;
+    // Gets the pointer to the stack variable
+    long long int stack_pointer = (long long int)&stack_variable;
+    // Check if it is on the heap
+     return (stack_pointer<variable_testing);
 }
