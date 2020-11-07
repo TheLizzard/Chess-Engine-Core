3ed0d859c241fe35e241fbcc6204529d5ebdd7f3
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-11-07 22:34:38 +0000

    Added some comments

--- scr/linkedlist.cpp
+++ scr/linkedlist.cpp
@@ -1,4 +1,3 @@
-// https://www.geeksforgeeks.org/reverse-a-linked-list/
 #pragma once
 #include <iostream>
 
@@ -7,6 +6,8 @@
 using namespace std;
 
 
+// The Node class:
+// Mostly the same as the `List` class
 template <class Type>
     class Node{
         public:
@@ -37,13 +38,6 @@ template <class Type>
                         return this->next->get(key-1);
                     }
                 }
-            }
-
-            int len(){
-                if (this->next == NULL){
-                    return 1;
-                }
-                return this->next->len()+1;
             }
 
             void append(Type data){
@@ -89,10 +83,17 @@ template <class Type>
             Node<Type>* iter_node = NULL;
 
             ~List(){
+                /*
+                Destructor
+                */
                 this->flush();
             }
 
             void deepcopy_to(List<Type>* _new){
+                /*
+                Deepcopies all of its attributes to `_new`.
+                Only works if list elements aren't on the heap.
+                */
                 _new->flush();
                 this->start_iter();
                 while (this->has_next()){
@@ -101,21 +102,36 @@ template <class Type>
             }
 
             void deepcopy_to_p(List<Type>* _new){
-                _new->flush_p();
+                /*
+                Deepcopies all of its attributes to `_new`.
+                Only works if list elements are on the heap.
+                */
+                _new->flush_p(); // Remove everything from the other list
+                // Iterate over this list:
                 this->start_iter();
                 while (this->has_next()){
                     Type old_item = this->next_item();
+                    // The object must have the `create_new` method. It should
+                    // return a pointer to a new object created on the heap.
                     Type item = old_item->create_new();
+                    // The object must have the `deepcopy_to` method too.
                     old_item->deepcopy_to(item);
                     _new->append(item);
                 }
             }
 
             void free(){
+                /*
+                Frees all of the list's objects
+                */
                 this->flush_p();
             }
 
             void flush(){
+                /*
+                Removes all objects from the list. Only works if
+                list elements aren't on the heap.
+                */
                 this->stop_iter();
                 while (this->next != NULL){
                     Type value = this->pop();
@@ -123,6 +139,10 @@ template <class Type>
             }
 
             void flush_p(){
+                /*
+                Removes all objects from the list. Only works if
+                list elements are on the heap.
+                */
                 this->stop_iter();
                 while (this->next != NULL){
                     Type item = this->pop();
@@ -131,8 +151,12 @@ template <class Type>
             }
 
             void append(Type data){
-                this->length += 1;
+                /*
+                Appends an object to the list
+                */
+                this->length += 1; // add 1 to the length
                 if (this->next == NULL){
+                    // Create the head node:
                     this->next = new Node<Type>(data);
                 }else{
                     this->next->append(data);
@@ -140,21 +164,29 @@ template <class Type>
             }
 
             Type operator[](int key){
+                /*
+                Gets an object from the list
+                */
                 if (this->next == NULL){
                     throw IndexError();
                 }
                 return this->next->get(key);
             }
 
-            int reset_len(){
-                return this->next->len();
-            }
-
             int len(){
+                /*
+                Returns the length of the list
+                */
                 return this->length;
             }
 
             void remove(int idx){
+                /*
+                Removes an object from the list (specified by idx)
+                Important:
+                    Must not be in the middle of iterating over the list
+                    if so use `list.stop_iter()`
+                */
                 if (this->iter_node != NULL){
                     throw ListSizeChangedError();
                 }
@@ -165,13 +197,19 @@ template <class Type>
                 if (idx == 0){
                     Node<Type>* temp = this->next;
                     this->next = temp->next;
-                    delete temp;
+                    delete temp; // Delete the node from the heap
                 }else{
                     this->next->remove(idx-1);
                 }
             }
 
             Type pop(){
+                /*
+                Pops and returns the last item from the list.
+                Important:
+                    Must not be in the middle of iterating over the list
+                    if so use `list.stop_iter()`
+                */
                 if (this->iter_node != NULL){
                     throw ListSizeChangedError();
                 }
@@ -191,14 +229,25 @@ template <class Type>
             }
 
             void start_iter(){
+                /*
+                Starts iterating over the list.
+                */
                 this->iter_node = this->next;
             }
 
             void stop_iter(){
+                /*
+                Stops iterating over the list.
+                */
                 this->iter_node = NULL;
             }
 
             Type next_item(){
+                /*
+                Gets the next item in the list
+                Note:
+                    first use `list.start_iter()` to begin the iteration of the list
+                */
                 if (this->iter_node == NULL){
                     throw IterationError();
                 }
@@ -208,10 +257,17 @@ template <class Type>
             }
 
             bool has_next(){
+                /*
+                Checks if the iteration is finished.
+                */
                 return (not (this->iter_node == NULL));
             }
 
             void reverse(){
+                /*
+                Reverses the list. Partly taken from:
+                https://www.geeksforgeeks.org/reverse-a-linked-list/
+                */
                 Node<Type>* current = this->next;
                 Node<Type>* prev = NULL;
                 Node<Type>* next = NULL;
@@ -226,6 +282,9 @@ template <class Type>
             }
 
             void print(){
+                /*
+                Prints the list to stdout.
+                */
                 this->start_iter();
                 cout << "[";
                 while (this->has_next()){
@@ -239,6 +298,10 @@ template <class Type>
             }
 
             void print_p(){
+                /*
+                Prints the list to stdout.
+                This is for when `Type` is a pointer.
+                */
                 this->start_iter();
                 cout << "[";
                 while (this->has_next()){
